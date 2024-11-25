4e9005e8d0c91e18807abd0f9e0078d0c9d44887
MaxKtv <maximkutiaev@outlook.com>
2024-11-25 16:14:48 +1000

    ref settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = [
     'webserver',
     '127.0.0.1',
     'https://python-project-52-hhf9.onrender.com',
+    '0.0.0.0',
 ]
 
 
@@ -94,10 +95,10 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 
