c4f780e9651252cf80a37a31ad2044eb36d4a84a
Maxim <159209762+MaxKtv@users.noreply.github.com>
2024-12-17 20:13:51 +1000

    ref base.py

--- task_manager/mixins/auth.py
+++ task_manager/mixins/auth.py
@@ -1,92 +1,126 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib import messages
-from django.shortcuts import redirect
-from django.utils.translation import gettext_lazy as _
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+from django.utils import timezone
 
 
-class BaseAuthMixin:
-    """Базовый миксин для обработки аутентификации и ошибок доступа."""
-    permission_message = _("You are not authorized! Please log in.")
-    permission_url = reverse_lazy('login')
+# Константы для сообщений
+SUCCESS_MESSAGES = {
+'create': _('created'),
+'update': _('updated'),
+'delete': _('deleted')
+}
 
-    def get_login_url(self):
-        return getattr(self, 'login_url', reverse_lazy('login'))
 
-    def get_permission_url(self):
-        return getattr(self, 'permission_url', reverse_lazy('login'))
+class NamedModelMeta(models.base.ModelBase):
+"""Метакласс устанавливает verbose_name и verbose_name_plural"""
 
-    def redirect_with_error(self, error_message, url):
-        messages.error(self.request, error_message)
-        if not url:
-            url = reverse_lazy('login')
-        return redirect(url)
+def __new__(cls, name, bases, attrs):
+    # Создаем класс модели
+    new_class = super().__new__(cls, name, bases, attrs)
 
-    def handle_no_permission(self):
-        """Обработка случаев отсутствия разрешений."""
-        if not self.request.user.is_authenticated:
-            return self.redirect_with_error(
-                _("You are not authorized! Please log in."),
-                self.get_login_url()
-            )
-        return self.redirect_with_error(
-            self.permission_message, self.get_permission_url()
+    # Получаем или создаем Meta
+    meta = attrs.get('Meta', type('Meta', (), {}))
+
+    # Устанавливаем значения только если они не определены
+    if not hasattr(meta, 'verbose_name'):
+        meta.verbose_name = _(name)
+    if not hasattr(meta, 'verbose_name_plural'):
+        meta.verbose_name_plural = _(f'{name}s')
+
+    # Обновляем Meta в новом классе
+    new_class._meta.verbose_name = meta.verbose_name
+    new_class._meta.verbose_name_plural = meta.verbose_name_plural
+
+    return new_class
+
+
+class NamedModel(models.Model, metaclass=NamedModelMeta):
+"""Базовый класс для моделей с полями name и created_at"""
+name = models.CharField(
+    max_length=100,
+    unique=True,
+    verbose_name=_('Name'),
+    error_messages={
+        'unique': _("Object with this name already exists."),
+    }
+)
+created_at = models.DateTimeField(default=timezone.now,
+                                  editable=False,
+                                  verbose_name=_('Created at'))
+
+def __str__(self):
+    return self.name
+
+def delete(self, *args, **kwargs):
+    """Проверяет, можно ли удалить объект"""
+    if hasattr(self, 'task_set') and self.task_set.exists():
+        raise models.ProtectedError(
+            "Cannot delete object because it has related tasks",
+            self.task_set.all()
         )
+    super().delete(*args, **kwargs)
+
+class Meta:
+    abstract = True
+    unique_together = [['name']]
 
 
-class AuthRequiredMixin(LoginRequiredMixin, BaseAuthMixin):
-    """Миксин для страниц, требующих аутентификации."""
-    login_url = reverse_lazy('login')
+class BaseViewMixin:
+"""Базовый миксин для всех представлений"""
+title = None
+success_message = None
+action_type = 'create'  # По умолчанию создание
 
-
-class AuthMixin(BaseAuthMixin):
-    """Миксин для аутентификации только для POST-запросов."""
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated and request.method != 'GET':
-            return self.handle_no_permission()
-        return super().dispatch(request, *args, **kwargs)
-
-
-class BasePermissionMixin(BaseAuthMixin, UserPassesTestMixin):
-    """Базовый миксин для проверки прав."""
-    permission_message = _("You don't have sufficient permissions.")
-
-    def handle_no_permission(self):
-        """Обработка случаев отсутствия разрешений"""
-        if not self.request.user.is_authenticated:
-            return self.redirect_with_error(
-                _("You are not authorized! Please log in."),
-                self.get_login_url()
+def get_context_data(self, **kwargs):
+    """Добавляет заголовок в контекст"""
+    context = super().get_context_data(**kwargs)
+    if self.title is None:
+        model_name = self.model._meta.verbose_name
+        if hasattr(self, 'object') and self.object:
+            self.title = _('Edit {model_name}').format(
+                model_name=model_name
             )
-        return self.redirect_with_error(
-            self.permission_message, self.get_permission_url()
+        else:
+            self.title = _('Create {model_name}').format(
+                model_name=model_name
+            )
+    context['title'] = self.title
+    return context
+
+@property
+def template_name(self):
+    """Автоматически генерирует имя шаблона"""
+    if not hasattr(self, '_template_name'):
+        app_label = self.model._meta.app_label
+        model_name = self.model._meta.model_name
+        self._template_name = (
+            f'{app_label}/{model_name}{self.template_name_suffix}.html'
         )
+    return self._template_name
 
+def get_success_url(self):
+    """URL для перенаправления после успешной операции"""
+    if not hasattr(self, '_success_url'):
+        app_label = self.model._meta.app_label
+        # Для модели User из auth приложения используем users namespace
+        if app_label == 'auth' and self.model._meta.model_name == 'user':
+            self._success_url = reverse_lazy('users:list')
+        else:
+            self._success_url = reverse_lazy(f'{app_label}:list')
+    return self._success_url
 
-class UserPermissionMixin(BasePermissionMixin):
-    """Миксин для проверки прав на управление пользователями."""
-    permission_message = _("You don't have permissions to modify user")
-    permission_url = reverse_lazy('users:list')
-
-    def test_func(self):
-        user = self.get_object()
-        return self.request.user == user
-
-
-class SuperuserRequiredMixin(BasePermissionMixin):
-    """Миксин для проверки прав администратора."""
-    permission_message = _("Only superusers can do this.")
-    permission_url = reverse_lazy('home')
-
-    def test_func(self):
-        return self.request.user.is_superuser
-
-
-class TaskAuthorRequiredMixin(BasePermissionMixin):
-    """Миксин для проверки, является ли пользователь автором задачи."""
-    permission_message = _("A task can only be deleted by its author")
-    permission_url = reverse_lazy('tasks:list')
-
-    def test_func(self):
-        task = self.get_object()
-        return task.author == self.request.user
+def get_success_message(self, cleaned_data=None):
+    """Get success message for the view."""
+    if self.success_message is None:
+        model_name = self.model._meta.verbose_name
+        action = SUCCESS_MESSAGES.get(
+            self.action_type,
+            SUCCESS_MESSAGES['create']
+        )
+        self.success_message = _('{model_name} successfully '
+                                 '{action}').format(
+            model_name=model_name,
+            action=action
+        )
+    return self.success_message
