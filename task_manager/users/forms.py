ae85d06da129a2aefa4bc1eafdf9310cd90b952d
MaxKtv <maximkutiaev@outlook.com>
2024-12-18 18:01:23 +1000

    fix forms.py

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -2,11 +2,15 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from task_manager.mixins.forms import FormWidgetMixin
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('first_name', 'last_name')
+        labels = {
+            "username": _("Username"),
+        }
 
 
 class UserUpdateForm(FormWidgetMixin, forms.ModelForm):
