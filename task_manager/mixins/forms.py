368293a58c394500f1b4348760f6d6d8325df4d9
MaxKtv <maximkutiaev@outlook.com>
2024-12-18 21:18:51 +1000

    fix full names in tasks

--- task_manager/mixins/forms.py
+++ task_manager/mixins/forms.py
@@ -24,3 +24,17 @@ class BaseNameModelForm(forms.ModelForm):
         label=_('Name'),
         widget=forms.TextInput(attrs=FORM_CONTROL_ATTRS)
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Пример кастомизации для ModelChoiceField
+        for field_name, field in self.fields.items():
+            if isinstance(field, forms.ModelChoiceField):
+                field.label_from_instance = self.get_label_from_instance
+
+    def get_label_from_instance(self, obj):
+        """Настраиваем отображение вариантов для ModelChoiceField"""
+        if hasattr(obj, 'first_name') and hasattr(obj, 'last_name'):
+            full_name = f"{obj.first_name} {obj.last_name}".strip()
+            return full_name if full_name else obj.username
+        return str(obj)
