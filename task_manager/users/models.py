01b7bc35fe750adf82304f881f32183d9b2d19c1
Maxim <159209762+MaxKtv@users.noreply.github.com>
2024-12-19 16:13:41 +1000

    Delete task_manager/users/models.py

--- task_manager/users/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-
-
-class CustomUser(User):
-    def __str__(self):
-        full_name = " ".join(filter(None, [self.first_name, self.last_name]))
-        return full_name if full_name else self.username
