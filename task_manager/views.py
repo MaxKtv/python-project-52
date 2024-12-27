e53ebc4201765064666ea8a3a9ee2d99bdb8ee24
MaxKtv <maximkutiaev@outlook.com>
2024-12-27 11:31:57 +1000

    ref views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,12 +1,5 @@
-import rollbar
-from django.http import HttpResponseServerError
 from django.views.generic.base import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = "index.html"
-
-
-def test_rollbar_error(request):
-    rollbar.report_message("Test error from task manager", "error")
-    return HttpResponseServerError("Test error page")
